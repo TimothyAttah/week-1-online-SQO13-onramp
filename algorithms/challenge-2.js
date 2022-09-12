1770f433e03cc762aa09dbd678fee36f39f50e4a
Timothy Attah <timothyattah733@gmail.com>
2022-09-12 10:36:24 +0100

    Fixed fizzBuzz challenge issue

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,6 +1,6 @@
 function fizzBuzz(input) {
   if (input % 5 === 0 && input % 3 === 0) {
-    return 'fizzbuzz';
+    return 'fizzBuzz';
   } else if (input % 5 === 0) {
     return 'buzz';
   } else if (input % 3 === 0) {
